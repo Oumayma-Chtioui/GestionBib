022805054f4a4db94b85e4306fd6313a850c7bc3
Oumayma-Chtioui <oumayma0203@gmail.com>
2024-12-04 16:26:31 +0100

    UpdateRent  Book.java

--- Book.java
+++ Book.java
@@ -3,8 +3,12 @@ enum GenreB{Fiction,NonFiction, Poésie, BD, Enfant}
 public class Book extends Article{
 	String author;
 	GenreB genre;
-	
-	public Book(String  author, GenreB genre, String t, String r, float p) {
+	public Book(String  author, GenreB genre, String t, String r, int d) {//d=duree
+		super(t,r,d);
+		this.author=author;
+		this.genre=genre;
+	}
+	public Book(String  author, GenreB genre, String t, String r, float p) {//p=prix
 		super(t,r,p);
 		this.author=author;
 		this.genre=genre;
